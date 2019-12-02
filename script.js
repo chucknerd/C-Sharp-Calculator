b2b6fa4e79a313c989f224da42a458065146f1dd
chucknerd <chucknerd1@gmail.com>
2019-12-02 06:01:01 -0600

    JS

---
+++ script.js
@@ -0,0 +1,111 @@
+const btn1 =
+document.getElementById("btn1");
+
+btn1.addEventListener("click", () => {
+    alert("button one called");
+});
+
+const btn2 =
+document.getElementById("btn2");
+
+btn1.addEventListener("click", () => {
+    alert("button two called");
+});
+
+const btn3 =
+document.getElementById("btn3");
+
+btn1.addEventListener("click", () => {
+    alert("button three called");
+});
+
+const btn4 =
+document.getElementById("btn4");
+
+btn1.addEventListener("click", () => {
+    alert("button four called");
+});
+
+const btn5 =
+document.getElementById("btn5");
+
+btn1.addEventListener("click", () => {
+    alert("button five called");
+});
+
+const btn6 =
+document.getElementById("btn6");
+
+btn1.addEventListener("click", () => {
+    alert("button six called");
+});
+
+const btn7 =
+document.getElementById("btn7");
+
+btn1.addEventListener("click", () => {
+    alert("button seven called");
+});
+
+const btn8 =
+document.getElementById("btn8");
+
+btn1.addEventListener("click", () => {
+    alert("button eight called");
+});
+
+const btn9 =
+document.getElementById("btn9");
+
+btn1.addEventListener("click", () => {
+    alert("button nine called");
+});
+
+const btn0 =
+document.getElementById("btn0");
+
+btn1.addEventListener("click", () => {
+    alert("button zero called");
+});
+
+const btnac =
+document.getElementById("btnac");
+
+btn1.addEventListener("click", () => {
+    alert("button all clear called");
+});
+
+const btnadd =
+document.getElementById("btnadd");
+
+btn1.addEventListener("click", () => {
+    alert("button add called");
+});
+
+const btnsub =
+document.getElementById("btnsub");
+
+btn1.addEventListener("click", () => {
+    alert("button substract called");
+});
+
+const btnmul =
+document.getElementById("btnmul");
+
+btn1.addEventListener("click", () => {
+    alert("button multiply called");
+});
+
+const btndiv =
+document.getElementById("btndiv");
+
+btn1.addEventListener("click", () => {
+    alert("button divide called");
+});
+
+const btnequ =
+document.getElementById("btnequ");
+
+btn1.addEventListener("click", () => {
+    alert("button equal called");
+});

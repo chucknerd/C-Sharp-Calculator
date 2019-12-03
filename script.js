10a65edd4ee1a1b56decaadb741c1748102d60bc
chucknerd <chucknerd1@gmail.com>
2019-12-03 06:04:07 -0600

    javascript and html updates

--- script.js
+++ script.js
@@ -1,111 +1,41 @@
-const btn1 =
-document.getElementById("btn1");
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+  };
+  
+  function updateDisplay() {
+    const display = document.querySelector('.calculator-screen');
+    display.value = calculator.displayValue;
+  }
+  
+  updateDisplay();
+  
+  const keys = document.querySelector(".keys");
+  keys.addEventListener("click", (event) => {
+      const {target} = event;
+      if (!target.matches('button')) {
+          return;
+      }
 
-btn1.addEventListener("click", () => {
-    alert("button one called");
-});
+      if (target.classList.contains('operator')) {
+          console.log('operator', target.value);
+          return;
+      }
 
-const btn2 =
-document.getElementById("btn2");
+      if (target.classList.contains('decimal')) {
+          console.log('decimal', target.value);
+          return;
+      }
 
-btn1.addEventListener("click", () => {
-    alert("button two called");
-});
+      if (target.classList.contains('all-clear')) {
+          console.log('all-clear', target.value);
+          return;
+      }
+          console.log('digit', target.value);
+      
+  });
 
-const btn3 =
-document.getElementById("btn3");
-
-btn1.addEventListener("click", () => {
-    alert("button three called");
-});
-
-const btn4 =
-document.getElementById("btn4");
-
-btn1.addEventListener("click", () => {
-    alert("button four called");
-});
-
-const btn5 =
-document.getElementById("btn5");
-
-btn1.addEventListener("click", () => {
-    alert("button five called");
-});
-
-const btn6 =
-document.getElementById("btn6");
-
-btn1.addEventListener("click", () => {
-    alert("button six called");
-});
-
-const btn7 =
-document.getElementById("btn7");
-
-btn1.addEventListener("click", () => {
-    alert("button seven called");
-});
-
-const btn8 =
-document.getElementById("btn8");
-
-btn1.addEventListener("click", () => {
-    alert("button eight called");
-});
-
-const btn9 =
-document.getElementById("btn9");
-
-btn1.addEventListener("click", () => {
-    alert("button nine called");
-});
-
-const btn0 =
-document.getElementById("btn0");
-
-btn1.addEventListener("click", () => {
-    alert("button zero called");
-});
-
-const btnac =
-document.getElementById("btnac");
-
-btn1.addEventListener("click", () => {
-    alert("button all clear called");
-});
-
-const btnadd =
-document.getElementById("btnadd");
-
-btn1.addEventListener("click", () => {
-    alert("button add called");
-});
-
-const btnsub =
-document.getElementById("btnsub");
-
-btn1.addEventListener("click", () => {
-    alert("button substract called");
-});
-
-const btnmul =
-document.getElementById("btnmul");
-
-btn1.addEventListener("click", () => {
-    alert("button multiply called");
-});
-
-const btndiv =
-document.getElementById("btndiv");
-
-btn1.addEventListener("click", () => {
-    alert("button divide called");
-});
-
-const btnequ =
-document.getElementById("btnequ");
-
-btn1.addEventListener("click", () => {
-    alert("button equal called");
-});
+  
+    //https://freshman.tech/calculator/
